978ecb9258c51056ff97029775c2e0db267a7af9
PabloRafael-coder <pabloofael@gmail.com>
2024-03-11 22:04:04 -0300

    Created app React

---
+++ src/my-portfolio/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styled';
+
+function Home() {
+  return (
+    <Container>
+      <h1>Item renderizado</h1>
+    </Container>
+  );
+}
+
+export default Home;
