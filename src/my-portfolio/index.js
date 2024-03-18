ecd7e2b27c36e639f9172975b2347b49628e8d07
PabloRafael-coder <pabloofael@gmail.com>
2024-03-18 18:27:51 -0300

    nusharge

--- src/my-portfolio/index.js
+++ src/my-portfolio/index.js
@@ -1,19 +1,19 @@
 import React from 'react';
 
-import { Container, Header, Navbar } from './styled';
+import { Container, Header, Navbar, ListOR } from './styled';
 
 function Home() {
   return (
     <Container>
       <Header>
         <p>Item renderizado</p>
-
         <p>Item renderizado</p>
         <Navbar>
-          <p>Home</p>
-          <p>Sobre</p>
-          <p>Projetos</p>
-          <p>Login</p>
+            <ListOR>
+                <li><a href="/">Home</a></li>
+                <li><a href="/sobre">Sobre</a></li>
+                <li><a href="/contato">Contato</a></li>
+            </ListOR>
         </Navbar>
       </Header>
       
--- src/my-portfolio/styled.js
+++ src/my-portfolio/styled.js
@@ -9,7 +9,7 @@ export const Header = styled.header`
     align-items: center;
     justify-content: space-around;
     width: 100%;
-    height: 10vh;
+    height: 60px;
     border: 1px solid black;
 `;
 
@@ -17,3 +17,20 @@ export const Navbar = styled.div`
     display: flex;
     gap: 10px;
 `;
+
+export const ListOR = styled.ul `
+    display: flex;
+
+    list-style: none;
+    gap: 20px;
+    
+
+    a { 
+        font-weight: 700;
+        text-decoration: none;
+        color: #000000;
+
+    }
+`;
+
+
